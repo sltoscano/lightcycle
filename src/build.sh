e90a1e62384ea55921056bcec29e2c8e97f30c96
trisource <trisource@trisource>
2013-01-06 22:11:29 -0800

    Adding dependencies for GL.

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-g++ -I /Users/trisource/projects/lightcycle/contrib/include/ -L /Users/trisource/projects/lightcycle/contrib/lib/mac64/ -lBox2D-debug -lboost_system -lboost_thread -lstdc++ server/main.cpp -o ./game_server
-g++ -I /Users/trisource/projects/lightcycle/contrib/include/ -L /Users/trisource/projects/lightcycle/contrib/lib/mac64/ -lboost_system -lboost_thread -lstdc++ client/main.cpp -o ./game_client
+echo Building server.
+g++ -I /usr/X11/include/ -I /Users/trisource/projects/lightcycle/contrib/include/ -L /Users/trisource/projects/lightcycle/contrib/lib/mac64/ -lBox2D-debug -lboost_system -lboost_thread -lstdc++ common/*.cpp server/*.cpp -o ./game_server
+echo Building client.
+g++ -I /usr/X11/include/ -I /Users/trisource/projects/lightcycle/contrib/include/ -L /usr/X11/lib -L /Users/trisource/projects/lightcycle/contrib/lib/mac64/ -lGL -lfreeglut-debug -lBox2D-debug -lboost_system -lboost_thread common/*.cpp render/*.cpp entity/*.cpp client/*.cpp -o ./game_client
